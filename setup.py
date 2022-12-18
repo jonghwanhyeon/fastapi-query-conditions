d28bae48900ecf1bf697ceb8fe6b8a829c228320
Jonghwan Hyeon <hyeon0145@gmail.com>
2022-12-18 14:31:59 +0900

    Update metadata

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open('README.md', 'r') as input_file:
-    long_description = input_file.read()
-
-setup(
-    name='fastapi-query-conditions',
-    version='1.0.1',
-    description='FastAPI-Query-Conditions is a dependency that parses a query string into conditions using operators enclosed in square brackets',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/jonghwanhyeon/fastapi-query-conditions',
-    author='Jonghwan Hyeon',
-    author_email='jonghwanhyeon93@gmail.com',
-    license='MIT',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Home Automation',
-    ],
-    keywords=['fastapi', 'query', 'querystring', 'condition'],
-    packages=find_packages(),
-    install_requires=['fastapi'],
-)
+setup()
